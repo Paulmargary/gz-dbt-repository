31f9f16af09b85ccf9c52f282083ef7850f9dd4d
paulmargary1@gmail.com <paulmargary1@gmail.com>
2025-02-11 15:26:03 +0000

    comit

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,19 @@
+WITH nanana AS(
+  SELECT *
+  from {{ ref("stg_raw__ship")}}
+  join {{ ref("int_sales_margin")}}
+  USING (orders_id)
+)
+
+
+SELECT orders_id
+    ,date_date
+    ,ROUND(SUM(margin),2) as margin
+    ,ROUND(SUM(shipping_fee),2) as shipping_fee
+    ,ROUND(SUM(logcost),2) as logcost
+    ,ROUND(SUM(ship_cost),2) as ship_cost
+    ,ROUND(SUM(margin) +SUM(shipping_fee) -SUM(logcost)- SUM(ship_cost),2) AS operational_margin
+FROM nanana
+GROUP BY orders_id, date_date
+ORDER BY orders_id, date_date
+
