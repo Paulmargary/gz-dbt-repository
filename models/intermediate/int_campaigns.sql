aa33542cf762774e56f528c2bb513729f952dd6d
paulmargary1@gmail.com <paulmargary1@gmail.com>
2025-02-12 15:39:26 +0000

    commut

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,12 +1,12 @@
-SELECT *
-FROM {{ ref("stg_raw__adwords")}}
-UNION ALL
-SELECT *
-FROM {{ ref("stg_raw__bing")}}
-UNION ALL
-SELECT *
-FROM {{ ref("stg_raw__criteo")}}
-UNION ALL
-SELECT *
-FROM {{ ref("stg_raw__facebook")}}
 
+  SELECT *
+  FROM {{ ref("stg_raw__adwords")}}
+  UNION ALL
+  SELECT *
+  FROM {{ ref("stg_raw__bing")}}
+  UNION ALL
+  SELECT *
+  FROM {{ ref("stg_raw__criteo")}}
+  UNION ALL
+  SELECT *
+  FROM {{ ref("stg_raw__facebook")}}
--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -5,10 +5,12 @@ SELECT
   ,ROUND(SUM(revenue) ,2)AS revenue
   ,ROUND(SAFE_DIVIDE(SUM(revenue), COUNT(orders_id)),2) AS avg_basket
   ,ROUND(SUM(operational_margin),2) AS operational_margin
-  ,ROUND(SUM(purchase_cost),2) AS total_pruchase_cost
+  ,ROUND(SUM(purchase_cost),2) AS total_purchase_cost
   ,ROUND(SUM(shipping_fee),2) AS total_shipping_fee
-  ,ROUND(SUM(logcost),2) AS total_logcoost
+  ,ROUND(SUM(logcost),2) AS total_logcost
   ,ROUND(SUM(quantity),2) AS total_product_sold
+  ,ROUND(SUM(margin)) AS margin
+  ,ROUND(sum(ship_cost)) AS ship_cost
 
 FROM {{ ref("int_orders_operational")}} 
 GROUP BY date_date
